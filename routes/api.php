5a8c40abc2119d7ec314ffd0dd8ee3e82c40d933
Rinaldy Pasya <rinaldy.pasya@silentmode.my>
2025-05-12 20:53:16 +0700

    chore: register middleware and define patient API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PatientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth.accessKey')->prefix('patients')->group(function () {
+    Route::post('/', [PatientController::class, 'store']);
+    Route::put('/{id}', [PatientController::class, 'update']);
+    Route::delete('/{id}', [PatientController::class, 'destroy']);
+    Route::get('/', [PatientController::class, 'index']);
+    Route::get('/{id}', [PatientController::class, 'show']);
 });
