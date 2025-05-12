fb68a7d51fd54b9a1c6f1d117d37e3980e730d19
Rinaldy Pasya <rinaldy.pasya@silentmode.my>
2025-05-12 20:42:16 +0700

    feat: add id_type, id_no, gender, dob, and address fields to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,11 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('id_type')->nullable();
+            $table->string('id_no')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->date('dob')->nullable();
+            $table->text('address')->nullable();
         });
     }
 
